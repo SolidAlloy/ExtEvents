a6c4e333d5dbc03083ca3a76703b73dd0332eb02
Artem Perepelitsa <perepelartem@gmail.com>
2021-12-07 09:07:35 +0200

    Initial commit

---
+++ Runtime/ExtEvent`1.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtEvents
+{
+    using System;
+
+    [Serializable]
+    public class ExtEvent<T> : BaseExtEvent
+    {
+
+    }
+}
---
+++ Runtime/ExtEvent`2.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtEvents
+{
+    using System;
+
+    [Serializable]
+    public class ExtEvent<T1, T2> : BaseExtEvent
+    {
+
+    }
+}
